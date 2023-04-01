44119b4a343b29e62d31ac970a0779db86d5921e
Tony Dugué <duguetony@gmail.com>
2023-04-02 00:37:25 +0200

    build: create BaseTemplate component

---
+++ components/templates/base/BaseTemplate.mocks.ts
@@ -0,0 +1,14 @@
+import { IBaseTemplate } from './BaseTemplate';
+
+const base: IBaseTemplate = {
+  sampleTextProp: 'Hello world!',
+};
+
+const alt: IBaseTemplate = {
+  sampleTextProp: 'Hello alt world!',
+};
+
+export const mockBaseTemplateProps = {
+  base,
+  alt
+};
