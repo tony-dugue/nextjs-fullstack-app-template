69cea70cf74bf6a24c653ce5e866f0b0624a0646
Tony Dugué <duguetony@gmail.com>
2023-04-09 12:32:07 +0200

    style: update theme

--- components/utility/search/Search.tsx
+++ components/utility/search/Search.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 export interface ISearch {}
 
@@ -17,13 +17,17 @@ const Search: React.FC<ISearch> = () => {
     >
       <input
         type="text"
-        className="rounded-full border-2 w-5/6 sm:w-96 h-12 px-3"
+        className="rounded-full border-2 w-5/6 sm:w-128 h-12 px-3"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
       <div className="space-x-3">
-        <button type="submit" className="btn-primary">Google Search</button>
-        <button type="submit" className="btn-primary">I&apos;m Feeling Lucky</button>
+        <button type="submit" className="btn-primary">
+          Google Search
+        </button>
+        <button type="submit" className="btn-primary">
+          I&apos;m Feeling Lucky
+        </button>
       </div>
     </form>
   );
